4150100a935410ec954283dae0825871a843d5f0
HelioJr548 <heliojr548@gmail.com>
2023-04-03 12:58:12 -0300

    Ultimo desafio da lista finalizado

--- Primerios passos com JavaScript/03 - lista de exercicios/resolucao-desafios.js
+++ Primerios passos com JavaScript/03 - lista de exercicios/resolucao-desafios.js
@@ -54,3 +54,32 @@ if (imc < 18.5) {
 } else {
     console.log(`Obesidade Grave`)
 }
+
+/*
+    3) Elabore um algoritmo que calcule o que deve ser pago por um produto, considerando o preço normal de etiqueta e a escolha da condição de pagamento. 
+    Utilize os códigos da tabela a seguir para ler qual a condição de pagamento escolhida e efetuar o cálculo adequado.
+
+    Código Condição de pagamento:
+    - À vista Débito, recebe 10% de desconto;
+    - À vista no Dinheiro ou PIX, recebe 15% de desconto;
+    - Em duas vezes, preço normal de etiqueta sem juros;
+    - Acima de duas vezes, preço normal de etiqueta mais juros de 10%;
+*/
+
+const precoEtiqueta = 10.00;
+const descontoAVistaDebito = precoEtiqueta * 0.1;
+const descontoAVistaDinheiroOuPix = precoEtiqueta * 0.15;
+const emDuasVezesSemJuros = precoEtiqueta;
+const acimaDuasVezesComJuros = precoEtiqueta * 0.1;
+const formatoPagamento = 4
+
+if (formatoPagamento === 1) {
+    console.log(precoEtiqueta - descontoAVistaDebito);
+} else if (formatoPagamento === 2) {
+    console.log(precoEtiqueta - descontoAVistaDinheiroOuPix);
+} else if (formatoPagamento === 3) {
+    console.log(precoEtiqueta);
+} else {
+    console.log(precoEtiqueta + acimaDuasVezesComJuros);
+}
+
