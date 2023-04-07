1af232eaa2eb382a7d3a65ed0393e1cf89841ac5
HelioJr548 <heliojr548@gmail.com>
2023-04-07 12:06:15 -0300

    Criando Classe

---
+++ dotNet-Csharp/FundamentosCsharp/Models/Pessoa.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FundamentosCsharp.Models
+{
+    public class Pessoa
+    {
+        
+    }
+}
