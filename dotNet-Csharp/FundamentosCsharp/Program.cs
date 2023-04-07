d4ca0bc92e6bf66e925a9ff0ec39c324387004ab
HelioJr548 <heliojr548@gmail.com>
2023-04-07 20:21:26 -0300

    Declarando variaveis

--- dotNet-Csharp/FundamentosCsharp/Program.cs
+++ dotNet-Csharp/FundamentosCsharp/Program.cs
@@ -1,4 +1,5 @@
-﻿using FundamentosCsharp.Models;
+﻿/*
+using FundamentosCsharp.Models;
 
 // Instanciando Classe
 Pessoa p = new Pessoa();
@@ -9,3 +10,17 @@ p.Idade = 19;
 
 // Chamando Método
 p.Apresentar();
+ */
+
+// DECLARANDO VARIAVEIS: tipo nomeVariavel = valor
+string apresentacao = "Olá, seja bem vindo";
+int quantidade = 1;
+double altura = 1.80;
+decimal preco = 1.80M;
+bool condicao = true;
+
+Console.WriteLine(apresentacao);
+Console.WriteLine($"Valor da variavel quantidade: {quantidade}");
+Console.WriteLine($"Valor da variavel altura: {altura}");
+Console.WriteLine($"Valor da variavel preco: {preco}");
+Console.WriteLine($"Valor da variavel condicao: {condicao}");
