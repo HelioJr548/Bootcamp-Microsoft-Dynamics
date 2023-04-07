24c0d284b25e52b07bfb063c23b4af4649617f1a
HelioJr548 <heliojr548@gmail.com>
2023-04-07 12:42:20 -0300

    Passando dados e chamando Método

--- dotNet-Csharp/FundamentosCsharp/Program.cs
+++ dotNet-Csharp/FundamentosCsharp/Program.cs
@@ -1,3 +1,11 @@
 ﻿using FundamentosCsharp.Models;
 
+// Instanciando Classe
 Pessoa p = new Pessoa();
+
+// Passando Dados para a instancia
+p.Nome = "Helio";
+p.Idade = 19;
+
+// Chamando Método
+p.Apresentar();
