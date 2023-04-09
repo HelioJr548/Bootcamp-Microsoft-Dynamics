f2d752a41255d4f3f29d7ca9c66a8330df253c71
HelioJr548 <heliojr548@gmail.com>
2023-04-09 20:34:21 -0300

    Convertendo tipos de variaveis

--- dotNet-Csharp/FundamentosCsharp/Program.cs
+++ dotNet-Csharp/FundamentosCsharp/Program.cs
@@ -12,8 +12,9 @@ p.Idade = 19;
 p.Apresentar();
  */
 
-// DECLARANDO VARIAVEIS: tipo nomeVariavel = valor
 /* 
+// DECLARANDO VARIAVEIS: tipo nomeVariavel = valor
+
 string apresentacao = "Olá, seja bem vindo";
 int quantidade = 1;
 double altura = 1.80;
@@ -27,6 +28,7 @@ Console.WriteLine($"Valor da variavel preco: {preco}");
 Console.WriteLine($"Valor da variavel condicao: {condicao}");
  */
 
+/* 
 // UTILIZANDO O DateTime para pegar data e horas atuais
 DateTime dataHoraAtual = DateTime.Now;
 Console.WriteLine($"Data e Hora atual: {dataHoraAtual}");
@@ -38,3 +40,12 @@ Console.WriteLine($"Data adicionada 5 dias e Hora atual : {addDiasNaDataAtual}")
 // UTILIZANDO O DateTime e formatando saida
 Console.WriteLine($"DateTime formatado p/ mostrar somente a data : {addDiasNaDataAtual.ToString("dd/MM/yyyy")}");
 Console.WriteLine($"DateTime formatado p/ mostrar data e hora sem milissegundos : {addDiasNaDataAtual.ToString("dd/MM/yyyy HH:mm")}");
+ */
+
+// CONVERTENDO TIPOS DE VARIAVEIS (CAST/CASTING)
+int a = Convert.ToInt32("5"); //Convert consegue tratar valores null
+Console.WriteLine(a);
+int b = int.Parse("5"); //Parse não consegue tratar valores null
+Console.WriteLine(b);
+int inteiro = 5;
+string c = inteiro.ToString(); //ToString converte valores para string
