fdec8b40ac49f1bd532493b2cb88f922a33a1bf5
HelioJr548 <heliojr548@gmail.com>
2023-05-03 22:48:18 -0300

    Instanciando classes e utilizando seus metodos

--- dotNet-Csharp/Propriedades_Metodos_Construtores/Program.cs
+++ dotNet-Csharp/Propriedades_Metodos_Construtores/Program.cs
@@ -1,7 +1,19 @@
 ﻿using Propriedades_Metodos_Construtores.Model;
 
+//Instancindo Classes
 Pessoa p1 = new Pessoa();
 p1.Nome = "Helio";
 p1.Sobrenome = "Santos";
-p1.Idade = 19;
-p1.Apresentar();
+
+Pessoa p2 = new Pessoa();
+p2.Nome = "Joao";
+p2.Sobrenome = "Perez";
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+//Utilizando metodos da Classe Curso
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
