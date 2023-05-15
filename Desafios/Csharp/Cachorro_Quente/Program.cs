7e869bed453f1af71dd555215b9c10f490148e13
HelioJr548 <heliojr548@gmail.com>
2023-05-14 23:11:10 -0300

    Desafio C# (concluido) - Sequencia Secreta

---
+++ Desafios/Csharp/Cachorro_Quente/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+// uma string com os dois valores separados por espaço
+string[] entrada = Console.ReadLine().Split();
+int participantes = int.Parse(entrada[0]);
+int cachorrosQuentes = int.Parse(entrada[1]);
+
+//TODO: Calcular a média de cachorros-quentes consumidos pelos participantes.
+double media = (double)participantes / cachorrosQuentes;
+
+//TODO: Imprimir a média com duas casas decimais.
+//Dica: Pesquise sobre o método "ToString".
+Console.WriteLine(media.ToString("F2"));
