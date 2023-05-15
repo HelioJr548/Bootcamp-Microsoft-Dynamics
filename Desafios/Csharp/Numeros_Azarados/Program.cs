d76fbd93b61db0bbf0a71e01158f0e28ebb18968
HelioJr548 <heliojr548@gmail.com>
2023-05-14 21:53:07 -0300

    Desafio C# (concluido) - Numeros Azarados

---
+++ Desafios/Csharp/Numeros_Azarados/Program.cs
@@ -0,0 +1,17 @@
+﻿string numero = Console.ReadLine();
+int num = Convert.ToInt32(numero);
+
+//TODO: Descubra e imprima se o número é azarado.
+//Dica: O método "Contains" pode ser útil neste desafio.
+
+if (num >= 0)
+{
+    if (numero.Contains("13"))
+    {
+        Console.WriteLine($"{numero} es de Mala Suerte");
+    }
+    else
+    {
+        Console.WriteLine($"{numero} NO es de Mala Suerte");
+    }
+}
